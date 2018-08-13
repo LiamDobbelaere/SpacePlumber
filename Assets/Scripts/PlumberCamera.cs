75f986859da91ae15c06e44b602cb1f5050a99cf
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-08-14 01:19:26 +0200

    First commit

---
+++ Assets/Scripts/PlumberCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlumberCamera : MonoBehaviour {
+    public float lerpSpeed = 10f;
+
+    private Transform plumber;
+
+    // Use this for initialization
+    void Start () {
+        plumber = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        Vector2 currentPos = transform.position;
+
+        currentPos = Vector2.Lerp(currentPos, plumber.position, lerpSpeed * Time.fixedDeltaTime);
+
+        transform.position = currentPos;
+    }
+}
---
+++ Assets/Scripts/PlumberController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlumberController : MonoBehaviour {
+    private GameObject[] planets;
+    private Rigidbody2D rigidbody2d;
+
+	// Use this for initialization
+	void Start () {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+        rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        FixedGravityUpdate();
+    }
+
+    void FixedGravityUpdate()
+    {
+        rigidbody2d.AddForce(CalculateGravityVector());
+    }
+
+    /*GameObject FindClosestPlanet()
+    {
+        GameObject closestPlanet = null;
+        float closestDistance = Mathf.Infinity;
+        Vector2 currentPosition = transform.position;
+
+        foreach (GameObject planet in planets)
+        {
+            Vector2 vectorToTarget = (Vector2) planet.transform.position - currentPosition;
+            float distanceSquared = vectorToTarget.sqrMagnitude;
+
+            if (distanceSquared < closestDistance)
+            {
+                closestDistance = distanceSquared;
+                closestPlanet = planet;
+            }
+        }
+
+        return closestPlanet;
+    }*/
+
+    Vector2 CalculateGravityVector()
+    {
+        Vector2 gravityVector = new Vector2(0, 0);
+        float planetCount = 0f;
+
+        foreach (GameObject planet in planets)
+        {
+            Vector2 vectorToPlanet = planet.transform.position - transform.position;
+
+            //sqrMagnitude can be seen as the pull distance
+            //Should probably be multiplied with something rather than having this check
+            if (vectorToPlanet.sqrMagnitude < 16)
+            {
+                gravityVector += vectorToPlanet.normalized;
+                planetCount += 1f;
+            }
+        }
+
+        gravityVector /= planetCount;
+
+        return gravityVector;
+    }
+}
