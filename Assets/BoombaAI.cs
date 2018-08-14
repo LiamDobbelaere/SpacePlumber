c3957874781c34ec2d5d5f573455524ec1a87fa0
T <timothymailacct@gmail.com>
2018-08-13 20:29:26 -0500

    2D effector gravity and rotational movement, still needs a way to automatically orient the player to the planet. Somewhat glitchy on rapid movements.

---
+++ Assets/BoombaAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoombaAI : MonoBehaviour {
+    public GameObject Ground;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Ground.transform.position, Vector3.forward, 40 * Time.deltaTime * -1);
+    }
+}
---
+++ Assets/Scripts/PlumberUserControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlumberUserControl : MonoBehaviour {
+    public GameObject Ground;
+    public bool Jump;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if (Input.GetAxis("Horizontal") > 0.05 && Ground != null)
+        {
+            transform.RotateAround(Ground.transform.position, Vector3.forward, 40 * Time.deltaTime * -1);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetAxis("Horizontal") < -0.05 && Ground != null)
+        {
+            transform.RotateAround(Ground.transform.position, Vector3.forward, 40 * Time.deltaTime * 1);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown("space") && Jump == false)
+        {
+            Jump = true;
+            Debug.Log("ADD");
+            GetComponent<Rigidbody2D>().AddForce(transform.TransformDirection(Vector3.up) * Time.deltaTime * 40000);
+        }
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Planet")
+        {
+            Ground = collision.gameObject;
+            Jump = false;
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Planet")
+        {
+          
+        }
+    }
+}
