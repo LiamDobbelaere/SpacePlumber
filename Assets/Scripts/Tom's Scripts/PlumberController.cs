cbb12b204a20b165e819b00cce56f14bee0512c6
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-08-22 23:40:50 +0200

    Trying to make it into a game

--- Assets/Scripts/Tom's Scripts/PlumberController.cs
+++ Assets/Scripts/Tom's Scripts/PlumberController.cs
@@ -15,13 +15,20 @@ public class PlumberController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         FixedGravityUpdate();
+        FixedMovementUpdate();
+    }
+
+    void FixedMovementUpdate()
+    {
+        Vector2 forward = Vector2.Perpendicular(Physics2D.gravity);
+
+        Debug.DrawRay(transform.position, forward * 1f, Color.green);
     }
 
     void FixedGravityUpdate()
     {
         Physics2D.gravity = CalculateGravityVector();
-
-        //rigidbody2d.AddForce(CalculateGravityVector());
+        Debug.DrawRay(transform.position, Physics2D.gravity * 1f, Color.cyan);
     }
 
     /*GameObject FindClosestPlanet()
@@ -53,17 +60,28 @@ public class PlumberController : MonoBehaviour {
         foreach (GameObject planet in planets)
         {
             Vector2 vectorToPlanet = planet.transform.position - transform.position;
+            CircleCollider2D cc = planet.GetComponent<CircleCollider2D>();
+            float estimatedRadius = cc.bounds.extents.x * 1.25f;
+
+            Debug.DrawLine(planet.transform.position, (Vector2) planet.transform.position + Vector2.up * estimatedRadius, Color.magenta);
+
+            float attractionPower = estimatedRadius / Vector2.Distance(planet.transform.position, transform.position);
+            Debug.Log(attractionPower);
 
             //sqrMagnitude can be seen as the pull distance
             //Should probably be multiplied with something rather than having this check
-            if (vectorToPlanet.sqrMagnitude < 16)
-            {
-                gravityVector += vectorToPlanet.normalized * (planet.transform.localScale.magnitude * 0.2f);
+            //if (Vector2.Distance(planet.transform.position, transform.position) < estimatedRadius)
+            //{
+                Debug.DrawLine(transform.position, planet.transform.position, Color.gray);
+                gravityVector += vectorToPlanet.normalized * attractionPower * (planet.transform.localScale.magnitude * 0.2f);
                 planetCount += 1f;
-            }
+            //}
         } 
 
-        gravityVector /= planetCount;
+        if (planetCount > 0)
+        {
+            gravityVector /= planetCount;
+        }
 
         return gravityVector;
     }
